06b5d36c56021990250cc655ca870e094006b846
L00125864ATU <L00125864@atu.ie>
2022-11-01 12:27:09 +0000

    Create GrootDb.sql

---
+++ GrootDb.sql
@@ -0,0 +1,14 @@
+create database Groot22;
+
+CREATE TABLE Customer (
+    CustomerID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Email varchar(50)
+);
+
+
+CREATE TABLE Grants (
+    GrantID int,
+	Payout money, 
+);
