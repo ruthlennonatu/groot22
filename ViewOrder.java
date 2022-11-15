bf1a43b91ee321a099ea4c6fe03efd402685d9a0
l00145222 <l00145222@atu.ie>
2022-11-15 22:16:43 +0000

    Resolves #gh-66 by adding ViewOrder.java

---
+++ ViewOrder.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class findOrder {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+	
+		System.out.println("Enter the your #Order ID.");
+		int orderID = scan.nextInt();
