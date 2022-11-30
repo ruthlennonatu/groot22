8a67a5cfee4c1c5243f2c5f7f0edce5aab3a464d
L00152147-ATU <l00152147@atu.ie>
2022-11-30 13:56:42 +0000

    Added maven project

---
+++ mavenproject/src/main/java/com/groot22/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.groot22.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mavenproject/src/test/java/com/groot22/mavenproject/AppTest.java
@@ -0,0 +1,20 @@
+package com.groot22.mavenproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
