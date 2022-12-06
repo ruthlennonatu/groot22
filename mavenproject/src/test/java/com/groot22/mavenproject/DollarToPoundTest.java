75f3bfba86e58c70c9678274cffd584870cb4656
L00125864ATU <L00125864@atu.ie>
2022-12-06 19:05:14 +0000

    add dollar to pound to test if the pipeline auto checks code

---
+++ mavenproject/src/test/java/com/groot22/mavenproject/DollarToPoundTest.java
@@ -0,0 +1,22 @@
+package com.groot22.mavenproject;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class DollarToPoundTest
+{
+	
+	@Test
+    public void testAdd() 
+	{
+        Assert.assertEquals(0.82,DollarToPound.exchange(1));
+    }
+	
+//	@Test
+//    public void testAdd2() 
+//	{
+//        Assert.assertEquals(100,DollarToPound.exchange(82.00));
+//    }
+
+}
