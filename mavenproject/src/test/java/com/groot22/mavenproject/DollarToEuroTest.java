7eaf0105970464fd11ae4663d9b5df50eb278e66
L00152147-ATU <115587796+L00152147-ATU@users.noreply.github.com>
2022-12-14 12:46:33 +0000

    Update DollarToEuroTest.java

--- mavenproject/src/test/java/com/groot22/mavenproject/DollarToEuroTest.java
+++ mavenproject/src/test/java/com/groot22/mavenproject/DollarToEuroTest.java
@@ -10,19 +10,19 @@ public class DollarToEuroTest
 	@Test
     public void testAdd() 
 	{
-        Assert.assertEquals(0.97,DollarToEuro.exchange(1));
+        Assert.assertEquals(0.97,DollarToEuro.exchange(1),0.02);
     }
 	
 	@Test
     public void testAdd2() 
 	{
-        Assert.assertEquals(97.00,DollarToEuro.exchange(100));
+        Assert.assertEquals(97.00,DollarToEuro.exchange(100),0.02);
     }
 	
 	@Test
     public void testAdd3() 
 	{
-        Assert.assertEquals(48.50,DollarToEuro.exchange(50));
+        Assert.assertEquals(48.50,DollarToEuro.exchange(50),0.02);
     }
 
 }
